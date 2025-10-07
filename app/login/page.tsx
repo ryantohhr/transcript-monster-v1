390141d9f5153bf6127245f06bfe258841d2b2ec
Ryan Toh <ryantohhongrui@hotmail.com>
2025-10-07 11:16:51 +0800

    feat: build login page ui

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -1,9 +1,33 @@
-import { login, signup } from "./actions";
+import { CircleQuestionMark } from "lucide-react";
+import GoogleSignInButton from "@/components/GoogleSignInButton";
+import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
 
 export default function LoginPage() {
   return (
-    <form>
-      <button formAction={login}>Log in</button>
-    </form>
+    <div className="flex justify-center items-center min-h-svh">
+      <Card className="w-2/5 flex flex-col justify-center">
+        <CardHeader>
+          {" "}
+          <CardTitle className="text-center flex flex-col text-xl">
+            Log In with Google
+          </CardTitle>
+        </CardHeader>{" "}
+        <CardContent className="flex flex-col gap-1 w-full items-center">
+          <div className="flex flex-col items-center w-6/7">
+            <GoogleSignInButton />
+            <div className="flex justify-center gap-1 mx-auto text-gray-500 rounded-md p-3">
+              <CircleQuestionMark
+                size={15}
+                className="mt-0.5 overflow-visible"
+              />
+              <span className="text-sm">
+                We use Google for authentication: <br />
+                No passwords are collected by Transcript Monster.
+              </span>
+            </div>
+          </div>
+        </CardContent>
+      </Card>
+    </div>
   );
 }
